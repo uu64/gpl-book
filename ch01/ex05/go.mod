cf35f6a0cbb04e2fb45f48a9f0573a7539fe470b
uu64 <yusuke.t.0808@gmail.com>
2021-08-22 22:21:09 +0900

    add: ch01/ex05

---
+++ ch01/ex05/go.mod
@@ -0,0 +1,3 @@
+module github.com/uu64/gpl-book/ch01/ex05
+
+go 1.15
---
+++ ch01/ex05/lisajous.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+	"time"
+)
+
+var palette = []color.Color{color.White, color.Black, color.RGBA{0, 255, 0, 1}}
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
+	greenIndex = 2
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	lisajous(os.Stdout)
+}
+
+func fillRect(img *image.Paletted, col color.Color) {
+	rect := img.Rect
+	for h := rect.Min.Y; h < rect.Max.Y; h++ {
+		for v := rect.Min.X; v < rect.Max.X; v++ {
+			img.Set(v, h, col)
+		}
+	}
+}
+
+func lisajous(out io.Writer) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		fillRect(img, palette[blackIndex])
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), greenIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
