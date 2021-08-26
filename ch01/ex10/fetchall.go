7bbab7235ef384f38ef5917d9c66b6bf9cc05411
uu64 <yusuke.t.0808@gmail.com>
2021-08-26 23:50:01 +0900

    add: ch01/ex10

---
+++ ch01/ex10/fetchall.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch)
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	out, err := ioutil.TempFile(os.TempDir(), "fetchall_")
+	if err != nil {
+		ch <- fmt.Sprintf("while creating temp file: %v", err)
+		return
+	}
+	defer out.Close()
+
+	nbytes, err := io.Copy(out, resp.Body)
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s %s", secs, nbytes, out.Name(), url)
+}
---
+++ ch01/ex10/go.mod
@@ -0,0 +1,3 @@
+module github.com/uu64/gpl-book/ch01/ex10
+
+go 1.15
