b20ce9ad6cd3d2b876af342ab41f37b008b544d3
uu64 <yusuke.t.0808@gmail.com>
2021-08-29 00:00:58 +0900

    fix: ch01/ex10

--- ch01/ex10/fetchall.go
+++ ch01/ex10/fetchall.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"time"
@@ -12,8 +11,8 @@ import (
 func main() {
 	start := time.Now()
 	ch := make(chan string)
-	for _, url := range os.Args[1:] {
-		go fetch(url, ch)
+	for i, url := range os.Args[1:] {
+		go fetch(url, ch, fmt.Sprintf("%d_%d.out", time.Now().UnixNano(), i))
 	}
 	for range os.Args[1:] {
 		fmt.Println(<-ch)
@@ -21,7 +20,7 @@ func main() {
 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 }
 
-func fetch(url string, ch chan<- string) {
+func fetch(url string, ch chan<- string, output string) {
 	start := time.Now()
 	resp, err := http.Get(url)
 	if err != nil {
@@ -30,7 +29,7 @@ func fetch(url string, ch chan<- string) {
 	}
 	defer resp.Body.Close()
 
-	out, err := ioutil.TempFile(os.TempDir(), "fetchall_")
+	out, err := os.Create(output)
 	if err != nil {
 		ch <- fmt.Sprintf("while creating temp file: %v", err)
 		return
