ad32ed7386a129f82db59d18a60663aa160412c1
uu64 <yusuke.t.0808@gmail.com>
2021-09-18 23:57:20 +0900

    add: ch03/ex01

---
+++ ch03/ex01/surface.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	width, height = 600, 320
+	cells         = 100
+	xyrange       = 30.0
+	xyscale       = width / 2 / xyrange
+	zscale        = height * 0.4
+	angle         = math.Pi / 6
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle)
+
+func main() {
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay, aerr := corner(i+1, j)
+			bx, by, berr := corner(i, j)
+			cx, cy, cerr := corner(i, j+1)
+			dx, dy, derr := corner(i+1, j+1)
+			if aerr != nil || berr != nil || cerr != nil || derr != nil {
+				continue
+			}
+			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy)
+		}
+
+	}
+	fmt.Println("</svg>")
+}
+
+func corner(i, j int) (float64, float64, error) {
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	z := f(x, y)
+	if math.IsNaN(z) || math.IsInf(z, 0) {
+		return 0, 0, fmt.Errorf("the value is invalid: %g", z)
+	}
+
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy, nil
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y)
+	return math.Sin(r) / r
+}
