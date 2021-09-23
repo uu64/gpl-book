2f595c9f71f967581ba7d9f022ff6b0f07145151
uu64 <yusuke.t.0808@gmail.com>
2021-09-24 02:03:29 +0900

    add: ch03/ex11

---
+++ ch03/ex11/comma/comma.go
@@ -0,0 +1,42 @@
+package comma
+
+import "bytes"
+
+func comma(s string) string {
+	var frac, sign, dot []byte
+
+	sbuf := []byte(s)
+
+	if sbuf[0] == '+' || sbuf[0] == '-' {
+		sign = []byte{sbuf[0]}
+		sbuf = sbuf[1:]
+	}
+
+	if idx := bytes.Index(sbuf, []byte(".")); idx != -1 {
+		dot = []byte{'.'}
+		frac = sbuf[idx+1:]
+		sbuf = sbuf[:idx]
+	}
+
+	n := len(sbuf)
+	if n <= 3 {
+		return toString(sbuf, frac, sign, dot)
+	}
+	var buf bytes.Buffer
+	for i := 0; i < n; i++ {
+		if (n-i)%3 == 0 {
+			buf.WriteByte(',')
+		}
+		buf.WriteByte(sbuf[i])
+	}
+	return toString(buf.Bytes(), frac, sign, dot)
+}
+
+func toString(intg, frac, sign, dot []byte) string {
+	var buf bytes.Buffer
+	buf.Write(sign)
+	buf.Write(intg)
+	buf.Write(dot)
+	buf.Write(frac)
+	return buf.String()
+}
---
+++ ch03/ex11/comma/go.mod
@@ -0,0 +1,3 @@
+module github.com/uu64/gpl-book/ch03/ex11
+
+go 1.17
