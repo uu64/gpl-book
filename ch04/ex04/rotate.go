c5f7ecd4c2aa8303287c3aad63e2275414d30211
uu64 <yusuke.t.0808@gmail.com>
2021-09-26 17:26:29 +0900

    update: ch04/ex04

--- ch04/ex04/rotate.go
+++ ch04/ex04/rotate.go
@@ -2,19 +2,36 @@ package main
 
 import "fmt"
 
-func rotate(x []int, count int) []int {
-	if len(x) == 0 {
-		return x
+func rotate(x []int, count int) {
+	if len(x) != 0 {
+		count = count % len(x)
 	}
-	for i := 0; i < count; i++ {
-		x = append(x, x[i])
+	if len(x) == 0 || count == 0 {
+		return
 	}
-	return x[count:]
+
+	tmp := x
+	for len(tmp) > count {
+		for i := count - 1; i > -1; i-- {
+			tmp[i], tmp[len(tmp)-count+i] = tmp[len(tmp)-count+i], tmp[i]
+		}
+		tmp = tmp[:len(tmp)-count]
+	}
 }
 
 func main() {
-	slice := []int{0, 1, 2, 3, 4, 5}
-	fmt.Println(slice)
-	slice = rotate(slice, 2)
-	fmt.Println(slice)
+	slice1 := []int{0, 1, 2, 3, 4, 5}
+	fmt.Println(slice1)
+	rotate(slice1, 2)
+	fmt.Println(slice1)
+
+	slice2 := []int{0, 1}
+	fmt.Println(slice2)
+	rotate(slice2, 3)
+	fmt.Println(slice2)
+
+	slice3 := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice3)
+	rotate(slice3, 10)
+	fmt.Println(slice3)
 }
--- ch04/ex04/rotate_test.go
+++ ch04/ex04/rotate_test.go
@@ -25,12 +25,15 @@ func TestRotate(t *testing.T) {
 		{[]int{0, 1}, 3, []int{1, 0}},
 		{[]int{0, 1, 2, 3, 4, 5}, 0, []int{0, 1, 2, 3, 4, 5}},
 		{[]int{0, 1, 2, 3, 4, 5}, 2, []int{2, 3, 4, 5, 0, 1}},
+		{[]int{0, 1, 2, 3, 4, 5, 6}, 10, []int{3, 4, 5, 6, 0, 1, 2}},
 	}
 
 	for _, test := range tests {
-		got := rotate(test.input, test.count)
-		if !equal(got, test.result) {
-			t.Errorf("rotate(%v, %v), then %v\n", test.input, test.count, got)
+		tmp := make([]int, len(test.input))
+		copy(tmp, test.input)
+		rotate(test.input, test.count)
+		if !equal(test.input, test.result) {
+			t.Errorf("rotate(%v, %v), then %v\n", tmp, test.count, test.input)
 		}
 	}
 
