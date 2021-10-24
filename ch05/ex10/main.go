8aa9fc5a14121561b0a913601464508f0e7efd6e
uu64 <yusuke.t.0808@gmail.com>
2021-10-24 23:44:01 +0900

    add: ch05/ex10

---
+++ ch05/ex10/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+)
+
+var prereqs = map[string][]string{
+	"algorithms": {"data structures"},
+	"calculus":   {"linear algebra"},
+
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
+
+func main() {
+	for i, course := range topoSort(prereqs) {
+		fmt.Printf("%d:\t%s\n", i+1, course)
+	}
+}
+
+func topoSort(m map[string][]string) []string {
+	var order []string
+	seen := make(map[string]bool)
+	var visitAll func(item string)
+
+	visitAll = func(item string) {
+		if !seen[item] {
+			seen[item] = true
+			for _, i := range m[item] {
+				visitAll(i)
+			}
+			order = append(order, item)
+		}
+	}
+
+	for key := range m {
+		visitAll(key)
+	}
+	return order
+}
