f224a480d14072cd95cccd617550823f2fd3f1ae
uu64 <yusuke.t.0808@gmail.com>
2021-10-30 23:39:44 +0900

    add: ch05/ex14

---
+++ ch05/ex14/go.mod
@@ -0,0 +1,3 @@
+module github.com/uu64/gpl-book/ch05/ex14
+
+go 1.17
---
+++ ch05/ex14/tree.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+type FileObject struct {
+	path string
+	name string
+	mode int
+}
+
+const (
+	regular = iota
+	directory
+	symlink
+	block
+	character
+	socket
+	fifo
+)
+
+var tree = make(map[string][]FileObject)
+
+func breadthFirst(f func(item string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
+
+func crawl(absPath string) []string {
+	childDirs := []string{}
+
+	entries, err := os.ReadDir(absPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(entries) == 0 {
+		return childDirs
+	}
+
+	for _, e := range entries {
+		name := e.Name()
+		var mode int
+		switch e.Type().String()[0] {
+		case 'D':
+			mode = block
+		case 'c':
+			mode = character
+		case 'd':
+			mode = directory
+		case 'L':
+			mode = symlink
+		case 'S':
+			mode = socket
+		case 'p':
+			mode = fifo
+		case '-':
+			mode = regular
+		default:
+			log.Fatalf("invalid file mode: %s", e.Type().String())
+		}
+
+		obj := FileObject{absPath, name, mode}
+		tree[absPath] = append(tree[absPath], obj)
+		if obj.mode == directory {
+			childDirs = append(childDirs, filepath.Join(absPath, obj.name))
+		}
+	}
+	return childDirs
+}
+
+func drawTree(absPath string) {
+	indent := "│   "
+	branch := "├── "
+	indentLast := "    "
+	branchLast := "└── "
+
+	var draw func(child []FileObject, depth int, prefix string)
+	draw = func(child []FileObject, depth int, prefix string) {
+		for i, c := range child {
+			fmt.Print(prefix)
+			if i == len(child)-1 {
+				fmt.Print(branchLast)
+			} else {
+				fmt.Print(branch)
+			}
+
+			var suffix string
+			if c.mode == symlink {
+				dst, err := os.Readlink(c.name)
+				if err != nil {
+					suffix = "(symlink is broken)"
+				} else {
+					suffix = fmt.Sprintf("-> %s", dst)
+				}
+			}
+
+			fmt.Printf("%s %s\n", c.name, suffix)
+			if c.mode == directory {
+				if i == len(child)-1 {
+					draw(tree[filepath.Join(c.path, c.name)], depth+1, prefix+indentLast)
+				} else {
+					draw(tree[filepath.Join(c.path, c.name)], depth+1, prefix+indent)
+				}
+			}
+		}
+	}
+	fmt.Println(filepath.Base(absPath))
+	draw(tree[absPath], 1, "")
+}
+
+func main() {
+	dirname := "."
+	if len(os.Args) > 1 {
+		dirname = os.Args[1]
+	}
+
+	abs, err := filepath.Abs(dirname)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	breadthFirst(crawl, []string{abs})
+	drawTree(abs)
+}
