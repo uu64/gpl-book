1be22086c8d8b22ec3e20d3b75d5b2b86a0d3e60
uu64 <yusuke.t.0808@gmail.com>
2021-11-08 00:36:13 +0900

    add: ch07/ex04

---
+++ ch07/ex04/go.mod
@@ -0,0 +1,5 @@
+module github.com/uu64/gpl-book/ch07/ex04
+
+go 1.17
+
+require golang.org/x/net v0.0.0-20211105192438-b53810dc28af // indirect
---
+++ ch07/ex04/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
+
+// Reader implements the io.Reader
+type Reader struct {
+	doc string
+}
+
+// Read implements the interface of the io.Reader
+func (r *Reader) Read(p []byte) (n int, err error) {
+	d := []byte(r.doc)
+	if len(d) == 0 {
+		err = io.EOF
+		return
+	}
+	end := len(p)
+	if len(d) < len(p) {
+		end = len(d)
+	}
+	n = copy(p, d[:end])
+	r.doc = string(d[end:])
+	return
+}
+
+// NewReader returns a new Reader
+func NewReader(s string) *Reader {
+	return &Reader{s}
+}
+
+const doc = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My First HTML</title>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <h1>My First Heading</h1>
+    <p>My first paragraph.</p>
+    <h2>An Unordered HTML List</h2>
+    <ul>
+      <li>Coffee</li>
+      <li>Tea</li>
+      <li>Milk</li>
+    </ul>  
+    <h2>An Ordered HTML List</h2>
+    <ol>
+      <li>Coffee</li>
+      <li>Tea</li>
+      <li>Milk</li>
+    </ol> 
+  </body>
+  </html>
+`
+
+func main() {
+	r := NewReader(doc)
+
+	outline(r)
+}
+
+func outline(r io.Reader) error {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return err
+	}
+
+	forEachNode(doc, startElement, endElement)
+
+	return nil
+}
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
+
+var depth int
+
+func startElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+		depth++
+	}
+}
+
+func endElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		depth--
+		fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+	}
+}
