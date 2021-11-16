c4ae1d2c12beb37b50e9a58d2eb4e57b6f5e380b
uu64 <yusuke.t.0808@gmail.com>
2021-11-17 01:44:18 +0900

    update: ch07/ex16

--- ch07/ex16/assets/index.js
+++ ch07/ex16/assets/index.js
@@ -1,3 +1,32 @@
-window.onload = () => {
-  console.log("Hello, World!");
-};
+function input(v) {
+  document.getElementById("answer").textContent += v
+}
+
+function ce() {
+  document.getElementById("expr").textContent = ""
+  document.getElementById("answer").textContent = ""
+}
+
+function enter() {
+  var expr = document.getElementById("answer").textContent
+  fetch("http://localhost:8080/calc/", {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      expr: expr
+    }),
+  }).then(async res => {
+    if(!res.ok) {
+      throw await res.json()
+    }
+    return res.json()
+  }).then(res => {
+    document.getElementById("expr").textContent = res.expr + "="
+    document.getElementById("answer").textContent = res.answer
+  }).catch(err => {
+    console.error(err.message)
+    document.getElementById("answer").textContent = "Error!"
+  })
+}
--- ch07/ex16/main.go
+++ ch07/ex16/main.go
@@ -1,22 +1,78 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/uu64/gpl-book/ch07/ex16/eval"
 )
 
-// var indexPage = template.Must(template.ParseFiles("tmpl/index.tmpl"))
+type reqBody struct {
+	Expr string `json:"expr"`
+}
+
+type successRes struct {
+	Expr   string  `json:"expr"`
+	Answer float64 `json:"answer"`
+}
+
+type errorRes struct {
+	Message string `json:"message"`
+}
 
 func main() {
-	// handler := func(w http.ResponseWriter, r *http.Request) {
-	// 	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
-	// 	if err := indexPage.Execute(w, "test"); err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// }
 	http.Handle("/", http.FileServer(http.Dir("html/")))
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets/"))))
+	http.HandleFunc("/calc/", calc)
 	fmt.Println("listen and serve... http://localhost:8080")
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func calc(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(404)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	var reqBody reqBody
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		res, _ := json.Marshal(errorRes{"invalid request"})
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(res)
+		return
+	}
+
+	// parse
+	expr, err := eval.Parse(reqBody.Expr)
+	if err != nil {
+		res, _ := json.Marshal(errorRes{fmt.Sprintf("failed to parse the input: %v", err)})
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(res)
+		return
+	}
+
+	// error check
+	vars := make(map[eval.Var]bool)
+	if err = expr.Check(vars); err != nil {
+		res, _ := json.Marshal(errorRes{fmt.Sprintf("invalid expr: %v", err)})
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(res)
+		return
+	}
+
+	// Web電卓アプリでは変数利用は想定していない
+	if len(vars) > 0 {
+		res, _ := json.Marshal(errorRes{"variables are not supported"})
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(res)
+		return
+	}
+
+	env := eval.Env{}
+	res, _ := json.Marshal(successRes{reqBody.Expr, expr.Eval(env)})
+	w.Write(res)
 }
