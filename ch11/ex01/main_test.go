dcacab2cfde2b44360a9b35da44c0461c083826f
uu64 <yusuke.t.0808@gmail.com>
2022-01-05 21:16:25 +0900

    add: ch11/ex01

---
+++ ch11/ex01/main_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"testing"
+)
+
+const input = `100
+Hello
+世界
+aaa
+`
+
+func TestCharCount(t *testing.T) {
+	inr, inw, err := os.Pipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer inr.Close()
+
+	outr, outw, err := os.Pipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outr.Close()
+
+	errr, errw, err := os.Pipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer errr.Close()
+
+	inw.Write([]byte(input))
+	inw.Close()
+
+	os.Stdin = inr
+	os.Stdout = outw
+	os.Stderr = errw
+
+	main()
+	outw.Close()
+	errw.Close()
+
+	outbuf, err := ioutil.ReadAll(outr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	errbuf, err := ioutil.ReadAll(errr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pat := []string{
+		"'1'\t1",
+		"'0'\t2",
+		"'H'\t1",
+		"'e'\t1",
+		"'l'\t2",
+		"'o'\t1",
+		"'世'\t1",
+		"'界'\t1",
+		"'a'\t3",
+		"'\\\\n'\t4",
+		"1\t15",
+		"2\t0",
+		"3\t2",
+		"4\t0",
+	}
+
+	for _, p := range pat {
+		matched, _ := regexp.Match(p, outbuf)
+		if !matched {
+			t.Errorf("'%s' should be matched.", p)
+			t.Errorf("stdout: %s", outbuf)
+			t.Errorf("stderr: %s", errbuf)
+		}
+	}
+}
